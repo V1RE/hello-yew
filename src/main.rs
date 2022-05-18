0fd7fb355c5b5c17c9bb81ea32a7185f2103248e
Niels Mentink <niels@ment.ink>
2022-05-18 21:04:11 +0200

    ✨ Added basic Counter component

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use yew::{function_component, html, use_state, Callback};
+
+#[function_component(Counter)]
+fn counter() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |_| counter.set(*counter + 1))
+    };
+
+    html! {
+    <div>
+        <button {onclick}>{"Add one"}</button>
+        <p>
+            <b>{ "Current value: " }</b>
+            { *counter }
+        </p>
+    </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Counter>();
 }
